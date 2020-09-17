3c8f9c0f4a3b1c9d79e41b6d23ccafaabd92499e
nagadheeraj <nagadheeraj@gmail.com>
2020-09-17 14:46:46 +0530

    feat: System: Implement Kernel() and OperatingSystem() methods

--- src/system.cpp
+++ src/system.cpp
@@ -14,20 +14,24 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+System::System() {
+	kernel_ = LinuxParser::Kernel();
+	os_ = LinuxParser::OperatingSystem();
+}
+
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return os_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
